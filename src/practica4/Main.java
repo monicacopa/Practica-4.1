a62b10b4e11d01cb316f284c12b7b2e14dadb728
monicacopa <monixucopa@gmail.com>
2024-04-24 17:38:49 +0200

    Main.java

---
+++ src/practica4/Main.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica4;
+
+/*
+*
+* @author Monica Collado
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+        CCuenta miCuenta;
+        double saldoActual;
+        
+
+        miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = miCuenta.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+        
+    
+    }
+    
+    public static void probarIngresar(CCuenta c, int cantidad) throws Exception{
+        c.ingresar(cantidad);
+        
+    }
+    
+    public static void probarRetirar(CCuenta c, int cantidad) throws Exception{
+        c.retirar(cantidad);
+    }
+}
