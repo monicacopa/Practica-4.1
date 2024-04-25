8a9e6d05f5c2724bc25c13f63f9cb780916d4d8d
monicacopa <monixucopa@gmail.com>
2024-04-25 17:18:22 +0200

    Commit_Final

--- src/entidadFinanciera/Main.java
+++ src/entidadFinanciera/Main.java
@@ -2,7 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package practica4;
+package entidadFinanciera;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /*
 *
@@ -12,23 +15,30 @@ package practica4;
 public class Main {
 
     public static void main(String[] args) {
-        CCuenta miCuenta;
+        CCuenta objetoCuenta;
         double saldoActual;
+	System.out.println("Prueba de Commit 1");
         
 
-        miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = miCuenta.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
-        
-    
+        objetoCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = objetoCuenta.estado();
+        System.out.println("El saldo actual es "+ saldoActual );
     }
     
-    public static void probarIngresar(CCuenta c, int cantidad) throws Exception{
-        c.ingresar(cantidad);
-        
+    public static void probarIngresar(CCuenta c, int cantidad, int cantidadEsperada) {
+        try {
+            c.ingresar(cantidad);
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
-    public static void probarRetirar(CCuenta c, int cantidad) throws Exception{
-        c.retirar(cantidad);
+    public static void probarRetirar(CCuenta c, int cantidad, int cantidadEsperada) {
+        try {
+            c.retirar(cantidad);
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
+
